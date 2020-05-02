7892c8d60912df7a638e297ed862d6bedf3c62e7
mauricechouam <lansichouamou@gmail.com>
2020-05-01 21:04:54 -0700

    Added Fucntion to display Time

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,15 @@
-var m = moment();
+const m = moment();
 
 var currentday = $("#currentDay");
 var currentime = $("#currentime");
+var format= 
 
 currentday.text(m.format('LLLL').toString());
-currentime.text(m.format('LT').toString());
+currentime.text(m.format("LT").toString());
 
+for (var i = 1; i <= 5; i++){
+    var a = $("#ftime" + i).text(m.add(i, "h")).text(m.format('LT').toString());
+    var b = $("#ptime" + i).text(m.subtract("h",i)).text(m.format('LT').toString());
+   
+
+}
